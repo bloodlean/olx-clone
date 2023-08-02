dd115d42355f92a8861eeb7c4d9f6a1692880907
bloodlean <bloodlean7@gmail.com>
2023-08-02 19:31:33 +0300

    commit

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)z3sczjg=-ccxuw&#@4#$nu44e*m1(yhwb9o@(suhkc%0&)d$j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
